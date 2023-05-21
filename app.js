82602f9d49ece47095778c9911186df19bf6f4fe
dichsa <diegochilet99@gmail.com>
2023-05-21 18:57:28 +0200

    criterio 6 hecho

--- app.js
+++ app.js
@@ -44,3 +44,26 @@ app.get('/api/autores', (req, res) => {
         res.status(200).json({ autores: results });
     });
 });
+
+// Obtener un post
+app.get('/api/:id', (req, res) => {
+    const { id } = req.params;
+
+    const sql = 'SELECT posts.*, autores.nombre AS autor_nombre, autores.email AS autor_email, autores.imagen AS autor_imagen FROM posts JOIN autores ON posts.id_autor = autores.id_autor WHERE posts.id_post = ?';
+
+
+    conexion.query(sql, [id], (error, results) => {
+        if(error) {
+            console.log('Error retireving post:', error);
+            res.status(500).json({ message: 'Error retrieving post' });
+            return;
+        }
+
+        if(results.length === 0) {
+            res.status(404).json({ message: 'Post not found' });
+            return;
+        }
+
+        res.status(200).json({ post: results[0] });
+    });
+});
