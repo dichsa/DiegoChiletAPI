86c834eb2d8205cdce52a646c2691cc6457e2f6e
dichsa <diegochilet99@gmail.com>
2023-05-21 18:29:51 +0200

    llamada de todos los autores y posts

--- app.js
+++ app.js
@@ -15,3 +15,32 @@ const conexion = mysql.createConnection({
 app.listen(3000, function () {
     console.log('La aplicación está escuchando en el puerto 3000!');
   });
+
+  // Obtener todos los posts
+app.get('/api/posts', (req, res) => {
+    const sql = 'SELECT *,  autores.nombre AS autor_nombre, autores.email AS autor_email, autores.imagen AS autor_imagen FROM posts JOIN autores';
+
+
+    conexion.query(sql, (error, results) => {
+        if(error) {
+            console.log('Error retrieving posts:', error);
+            res.status(500).json({ message: 'Error retrieving posts' });
+            return;
+        }
+        res.status(200).json({ posts: results });
+    });
+});
+
+// Obtener todos los autores
+app.get('/api/autores', (req, res) => {
+    const sql = 'SELECT * FROM autores';
+
+    conexion.query(sql, (error, results) => {
+        if(error) {
+            console.log('Error retrieving autores:', error);
+            res.status(500).json({ message: 'Error retrieving autores' });
+            return;
+        }
+        res.status(200).json({ autores: results });
+    });
+});
